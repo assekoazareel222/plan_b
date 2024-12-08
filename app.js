4ba7bdd4f90b0aa2a7c3006108546bd9260f7ccd
ASSEKO <assekoazareel222@gmail.com>
2024-12-08 19:50:19 +0100

    port supprimer

--- app.js
+++ app.js
@@ -43,8 +43,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT || 3306  // Si pas de port spécifié, utiliser le port par défaut
+  database: process.env.DB_DATABASE, // Si pas de port spécifié, utiliser le port par défaut
 });
 
 
